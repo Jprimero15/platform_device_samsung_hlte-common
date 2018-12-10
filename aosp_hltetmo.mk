add275f2cc2729d64da7b5a7e59a0a79b3e6f1bb
Joshua Primero <jprimero155@gmail.com>
2018-12-10 08:58:25 +0000

    hltetmo: AEX-ify
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- aosp_hltetmo.mk
+++ aosp_hltetmo.mk
@@ -13,10 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit from hltetmo device
 $(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
 
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Include Bootanimation configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_DEVICE := hltetmo
-PRODUCT_NAME := lineage_hltetmo
+PRODUCT_NAME := aosp_hltetmo
