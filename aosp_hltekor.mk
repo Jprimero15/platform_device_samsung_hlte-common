db65a354eb3c2365ce3bdf09a30976e7f44a2054
Joshua Primero <jprimero155@gmail.com>
2018-12-10 08:52:40 +0000

    hltekor: AEX-ify
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- aosp_hltekor.mk
+++ aosp_hltekor.mk
@@ -13,10 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
+# Inherit from hltekor device
 $(call inherit-product, device/samsung/hltekor/full_hlte.mk)
 
+# Inherit some common AEX stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Include Bootanimation configuration
+TARGET_BOOT_ANIMATION_RES := 1080
+
 PRODUCT_DEVICE := hltekor
-PRODUCT_NAME := lineage_hltekor
+PRODUCT_NAME :=aosp_hltekor
